1b9db43f7c88f026896544be12b0d06b01479c9b
nayou <nayoung4300@naver.com>
2024-03-25 18:43:13 +0900

    project init

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,15 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
+  {
+    path: "/testcomponent",
+    name: "testcomponent",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/TestComponent.vue"),
+  },
 ];
 
 const router = createRouter({
---
+++ src/views/TestComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>테스트:{{ title }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      title: "동적 타이틀 이름",
+    };
+  },
+};
+</script>
